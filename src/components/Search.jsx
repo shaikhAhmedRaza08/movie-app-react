ce8fcbbb1ce1b7ef3eeb994d5a2f3d91521fac38
shaikhAhmedRaza08 <shaikhahmedraza72@outlook.com>
2025-06-10 15:48:29 +0530

    Add Search, Spinner and Movie Components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
-const Search = () => {
-    return (<div className="text-white text-3xl">Search</div>)
+const Search = ({searchTerm, setSearchTerm}) => {
+    return (<div className="search">
+        <div>
+            <img src="search.svg" alt="Search"/>
+
+            <input
+                type="text"
+                placeholder="Search through thousands of movies"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </div>
+    </div>)
 }
 export default Search
